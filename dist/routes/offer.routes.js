ffbbba5101b80bd271e8eb93ee1b84181f4601c3
Banavath Srinivasulu <srinivasnaik4400@gmail.com>
2026-06-05 10:52:50 +0530

    Remove dist and vscode from repository

--- dist/routes/offer.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const offer_controller_1 = require("../controllers/offer.controller");
-const auth_middleware_1 = require("../middlewares/auth.middleware");
-const router = (0, express_1.Router)();
-router.post('/', auth_middleware_1.authenticateJWT, offer_controller_1.createOffer);
-router.get('/', offer_controller_1.getOffers);
-exports.default = router;
